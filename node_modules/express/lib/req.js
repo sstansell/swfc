8fa9ed803755b19c267ea7f4b6682a39d5518009
Sean Stansell <seanstansell@gmail.com>
2014-12-01 10:24:42 -0500

    including node modules

---
+++ node_modules/express/lib/req.js
@@ -0,0 +1,23 @@
+var request = require('request');
+
+// Set the headers
+var headers = {
+    'User-Agent':       'Super Agent/0.0.1',
+    'Content-Type':     'application/x-www-form-urlencoded'
+}
+
+// Configure the request
+var options = {
+    url: 'http://samwize.com',
+    method: 'GET',
+    headers: headers,
+    qs: {'key1': 'xxx', 'key2': 'yyy'}
+}
+
+// Start the request
+request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        // Print out the response body
+        console.log(body)
+    }
+})
